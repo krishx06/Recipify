532992720949c75f3d7528d2666dcf2cc076ce9d
krishx06 <krishpatil120@gmail.com>
2025-12-01 08:27:36 +0530

    Updated backend files

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
-import healthRoutes from "./routes/healthRoutes.js";  
-import authRoutes from "./routes/authRoutes.js";       
+import healthRoutes from "./routes/healthRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import http from "http";
 import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import aiRoutes from "./routes/ai.routes.js";
 
 dotenv.config();
 connectDB();
+app.use("/ai", aiRoutes);
 
 const PORT = process.env.PORT || 5000;
 
